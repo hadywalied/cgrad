e00460083480a41de3b0d1cd1b1d1dbd3ae757bb
hadywalied <my.mepharo1230@gmail.com>
2025-09-25 00:45:30 +0300

    Refactor Value class to support autograd and labeling.
    Introduced gradients, labeling, and operations tracking in the `Value` class to enable auto-differentiation. Added `backward()` for gradient computation, overloads for arithmetic operators to propagate gradients, and `tanh()` with backward functionality. Updated example usage in `main.cpp` to leverage these new features.

--- engine.h
+++ engine.h
@@ -5,160 +5,153 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 #include <ostream>
-
+#include <cmath>
+#include <string>
+#include <utility>
+#include <vector>
+#include <set>
+#include <functional>
 
 template<typename T>
 class Value {
 public:
     T data;
-    float grad;
+    mutable double grad;
+    std::string label;
+    std::string operation;
+    std::vector<std::reference_wrapper<const Value<T>> > children;
+    std::function<void(Value<T> &)> _backward = [](Value<T> &) {
+    };
 
-    explicit Value(T data) : data(data), grad(0) {
+    explicit Value(T data, std::string label = "",
+                   std::string operation = "",
+                   std::vector<std::reference_wrapper<const Value<T>> > children = {}) : data(data), grad(0.0),
+        label(std::move(label)),
+        operation(std::move(operation)),
+        children(std::move(children)) {
     };
 
     template<typename U>
     friend std::ostream &operator<<(std::ostream &os, const Value<U> &value);
 
     Value<T> operator+(const Value<T> &other) const {
-        return Value<T>(data + other.data);
+        auto out = Value<T>(data + other.data, "", "+", {*this, other});
+        out._backward = [](Value<T> &value) {
+            value.children[0].get().grad += value.grad;
+            value.children[1].get().grad += value.grad;
+        };
+        return out;
     }
 
     Value<T> operator+(const T &other) const {
-        return Value<T>(data + other);
+        return *this + Value<T>(other);
     }
 
-    // scalar plus Value (needs to be a friend function)
     friend Value<T> operator+(const T &scalar, const Value<T> &value) {
-        return Value<T>(scalar + value.data);
+        return value + scalar;
     }
 
-
-    Value<T> operator-(const Value &other) const {
-        return Value<T>(data - other.data);
+    Value<T> operator-(const Value<T> &other) const {
+        auto out = Value<T>(data - other.data, "", "-", {*this, other});
+        out._backward = [](Value<T> &v) {
+            v.children[0].get().grad += v.grad;
+            v.children[1].get().grad -= v.grad;
+        };
+        return out;
     }
 
     Value<T> operator-(const T &other) const {
-        return Value<T>(data - other);
+        return *this - Value<T>(other);
     }
 
     friend Value<T> operator-(const T &scalar, const Value<T> &value) {
-        return Value<T>(scalar - value.data);
+        return Value<T>(scalar) - value;
     }
 
-    Value<T> operator*(const Value &other) const {
-        return Value<T>(data * other.data);
+    Value<T> operator*(const Value<T> &other) const {
+        auto out = Value<T>(data * other.data, "", "*", {*this, other});
+        out._backward = [](Value<T> &v) {
+            v.children[0].get().grad += v.children[1].get().data * v.grad;
+            v.children[1].get().grad += v.children[0].get().data * v.grad;
+        };
+        return out;
     }
 
     Value<T> operator*(const T &other) const {
-        return Value<T>(data * other);
+        return *this * Value<T>(other);
     }
 
     friend Value<T> operator*(const T &scalar, const Value<T> &value) {
-        return Value<T>(scalar * value.data);
+        return value * scalar;
     }
 
-    Value<T> operator^(const Value &other) const {
-        if (other.data == 0) {
-            return Value<T>(1);
-        }
-        if (other.data == 1) {
-            return Value<T>(data);
-        }
-        T result = 1;
-        T exponent = other.data;
-        T value = data;
-        // Handle negative exponents
-        if (exponent < 0) {
-            value = 1 / value;
-            exponent = -exponent;
-        }
-        // Use binary exponentiation for better efficiency
-        while (exponent > 0) {
-            if (exponent & 1) {
-                result *= value;
-            }
-            value *= value;
-            exponent >>= 1;
-        }
-        return Value<T>(result);
+    Value<T> operator^(const Value<T> &other) const {
+        auto out = Value<T>(std::pow(data, other.data), "", "^", {*this, other});
+        out._backward = [](Value<T> &v) {
+            v.children[0].get().grad += (v.children[1].get().data * std::pow(
+                                             v.children[0].get().data, v.children[1].get().data - 1)) * v.grad;
+            v.children[1].get().grad += (std::log(v.children[0].get().data) * std::pow(
+                                             v.children[0].get().data, v.children[1].get().data)) * v.grad;
+        };
+        return out;
     }
 
     Value<T> operator^(const T &other) const {
-        if (other == 0) {
-            return Value<T>(1);
-        }
-        if (other == 1) {
-            return Value<T>(data);
-        }
-        T result = 1;
-        T exponent = other;
-        T value = data;
-        // Handle negative exponents
-        if (exponent < 0) {
-            value = 1 / value;
-            exponent = -exponent;
-        }
-        // Use binary exponentiation for better efficiency
-        while (exponent > 0) {
-            if (exponent & 1) {
-                result *= value;
-            }
-            value *= value;
-            exponent >>= 1;
-        }
-        return Value<T>(result);
+        return *this ^ Value<T>(other);
     }
 
-    friend Value<T> operator^(const T &scalar, const Value<T> &value_obj_value) {
-        if (value_obj_value.data == 0) {
-            return Value<T>(1);
-        }
-        if (value_obj_value.data == 1) {
-            return Value<T>(scalar);
-        }
-        T result = 1;
-        T exponent = value_obj_value.data;
-        T value = scalar;
-        // Handle negative exponents
-        if (exponent < 0) {
-            value = 1 / value;
-            exponent = -exponent;
-        }
-        // Use binary exponentiation for better efficiency
-        while (exponent > 0) {
-            if (exponent & 1) {
-                result *= value;
-            }
-            value *= value;
-            exponent >>= 1;
-        }
-        return Value<T>(result);
+    friend Value<T> operator^(const T &scalar, const Value<T> &value) {
+        return Value<T>(scalar) ^ value;
     }
 
-    Value<T> operator/(const Value &other) const {
-        return (*this) * (other ^ (-1));
+    Value<T> operator/(const Value<T> &other) const {
+        return *this * (other ^ Value<T>(-1));
     }
-
 
     Value<T> operator/(const T &other) const {
-        return Value((*this).data / other);
+        return *this / Value<T>(other);
     }
 
     friend Value<T> operator/(const T &scalar, const Value<T> &value) {
-        return Value(scalar / value.data);
+        return Value<T>(scalar) / value;
     }
 
-    //     Value<T> tanh();
-    //
-    //     void backward();
-    //
-    // private:
-    //     void _backward();
+    Value<double> tanh() const {
+        auto vl = std::tanh(data);
+        auto out = Value<double>(vl, "tanh(" + this->label + ")", "tanh", {*this});
+        out._backward = [vl](Value<double> &v) {
+            v.children[0].get().grad += (1 - std::pow(vl, 2)) * v.grad;
+        };
+        return out;
+    };
+
+    void backward() {
+        std::vector<std::reference_wrapper<const Value<T>> > topo;
+        std::set<const Value<T> *> visited;
+
+        std::function<void(const Value<T> &)> build_topo = [&](const Value<T> &v) {
+            if (visited.find(&v) == visited.end()) {
+                visited.insert(&v);
+                for (const auto &child: v.children) {
+                    build_topo(child.get());
+                }
+                topo.push_back(v);
+            }
+        };
+
+        build_topo(*this);
+
+        grad = 1.0;
+        for (auto it = topo.rbegin(); it != topo.rend(); ++it) {
+            const_cast<Value<T> &>(it->get())._backward(const_cast<Value<T> &>(it->get()));
+        }
+    }
 };
 
 template<typename T>
 std::ostream &operator<<(std::ostream &os, const Value<T> &value) {
-    os << "Value(data=" << value.data << ", grad=" << value.grad << ")";
+    os << "Value(label= " << value.label << ", data=" << value.data << ", grad=" << value.grad << ", op= " << value.
+            operation << ")";
     return os;
 }
 
--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 int main() {
-    const auto a = Value(1);
-    const auto b = Value(1);
-    const auto c = a + b;
+    auto a = Value(1, "a");
+    auto b = Value(1, "b");
+    auto c = a + b;
+    c.label = "c";
 
-    cout <<"+"<< endl;
+    cout << "+" << endl;
     cout << "c = " << c << endl;
     cout << "c + 2 = " << c + 2 << endl;
     cout << "1 + c= " << 1 + c << endl;
@@ -32,5 +33,33 @@ int main() {
     cout << (c * 2) << endl;
     cout << (2 * c) << endl;
 
+    cout << "tanh" << endl;
+    auto d = Value(0.5, "d");
+    cout << d.tanh() << endl;
+
+
+    auto x = Value(2.0, "x");
+    auto y = Value(0.0, "y");
+    auto wx = Value(-3.0, "wx");
+    auto wy = Value(1.0, "wy");
+    auto bias = Value(6.88, "bias");
+    auto xwx = wx * x; xwx.label = "xwx";
+    auto ywy = wy * y; ywy.label = "ywy";
+    auto z = xwx + ywy; z.label = "x*wx + y*wy";
+    auto n = z + bias; n.label = 'n';
+    auto o = n.tanh(); o.label = 'o';
+    o.backward();
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "wx = " << wx << endl;
+    cout << "wy = " << wy << endl;
+    cout << "bias = " << bias << endl;
+    cout << "xwx = " << xwx << endl;
+    cout << "ywy = " << ywy << endl;
+    cout << "z = " << z << endl;
+    cout << "n = " << n << endl;
+    cout << "o = " << o << endl;
+    cout << "o.grad = " << o.grad << endl;
     return 0;
 }
